0ed0ebc407c56fb304c50aa359efc24f4ad8e770
csw <caisw11@chinaunicom.cn>
2020-06-17 17:08:43 +0800

    feat($compile): short
    Long desc

--- jdk8/src/main/java/com/chinaunicom/defaultmethod/DefaultMethodTest.java
+++ jdk8/src/main/java/com/chinaunicom/defaultmethod/DefaultMethodTest.java
@@ -1,5 +1,10 @@
 package com.chinaunicom.defaultmethod;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author csw
  * @date
@@ -9,5 +14,9 @@ public class DefaultMethodTest extends MyInterface1Impl implements  MyInterface2
     public static void main(String[] args) {
         DefaultMethodTest test = new DefaultMethodTest();
         test.defaultMethod();
+        List<Map<String,Object>> variables = new ArrayList<>();
+
+
     }
+
 }
